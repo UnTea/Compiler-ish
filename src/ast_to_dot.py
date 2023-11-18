7aa38ee274597ff415d69697d21d069853870b32
UnTea <weqqu2014@yandex.ru>
2023-11-18 17:40:47 +0300

    add file for graph representation

--- src/ast_to_dot.py
+++ src/ast_to_dot.py
@@ -1,39 +1,74 @@
 import ast
-import sys
+import os
+from collections import defaultdict
 
 
-def ast_to_dot(node, parent_id=-1):
-    if isinstance(node, ast.AST):
-        node_id = hash(node)
-        print(f'{node_id} [label="{type(node).__name__}"]')
+class ASTVisualizer:
+    def __init__(self):
+        self.graph = defaultdict(list)
+        self.labels = {}
 
-        if parent_id != -1:
-            print(f'{parent_id} -> {node_id}')
+    def ast_visitor(self, tree):
+        stack = [(None, tree)]
+        parent_id = None
 
-        for field, value in ast.iter_fields(node):
-            if isinstance(value, list):
-                for item in value:
-                    if isinstance(item, ast.AST):
-                        ast_to_dot(item, node_id)
+        while stack:
+            parent_id, current_node = stack.pop()
 
-            elif isinstance(value, ast.AST):
-                ast_to_dot(value, node_id)
+            if isinstance(current_node, ast.AST):
+                node_id = len(self.graph)
+                self.graph[node_id] = []
+                self.labels[node_id] = type(current_node).__name__
 
+                if parent_id is not None:
+                    self.graph[parent_id].append(node_id)
 
-def generate_dot_code(source_code):
-    tree = ast.parse(source_code)
-    print("digraph AST {")
-    ast_to_dot(tree)
-    print("}")
+                for field in reversed(current_node._fields):
+                    stack.append((node_id, getattr(current_node, field)))
+
+            elif isinstance(current_node, list):
+                for element in reversed(current_node):
+                    stack.append((parent_id, element))
+
+        return parent_id
+
+    def generate_dot_code(self):
+        print("digraph AST {")
+        self.print_nodes_dot(0)
+        print("}")
+
+    def print_nodes_dot(self, node_id, visited=None):
+        if visited is None:
+            visited = set()
+
+        if node_id in visited:
+            return
+
+        visited.add(node_id)
+        print(f'"{node_id}" [label="{self.labels[node_id]}"]')
+
+        for child_id in self.graph[node_id]:
+            print(f'"{node_id}" -> "{child_id}"')
+            self.print_nodes_dot(child_id, visited)
+
+    def load_from_file(self, file_path):
+        if not os.path.exists(file_path):
+            print(f"File not found: {file_path}")
+
+            return
+
+        with open(file_path, 'r') as f:
+            source_code = f.read()
+
+        tree = ast.parse(source_code)
+        self.ast_visitor(tree)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python ast_to_dot.py <source_file.py>")
-        sys.exit(1)
+    visualizer = ASTVisualizer()
 
-    source_file = sys.argv[1]
-    with open(source_file, 'r') as f:
-        source_code = f.read()
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    test_file_path = os.path.join(script_dir, "..", "tests", "test_file.py")
 
-    generate_dot_code(source_code)
+    visualizer.load_from_file(test_file_path)
+    visualizer.generate_dot_code()
